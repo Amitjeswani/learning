eaf32d02420e36d30126d5fb295945fcc15d5545
unknown <jeswani622@gmail.com>
2024-12-19 02:45:40 +0530

    deploy to docker

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker abc here</h2>'
+    return '<h1>Hello from Flask & Docker here </h2>'
 
 
 if __name__ == "__main__":
